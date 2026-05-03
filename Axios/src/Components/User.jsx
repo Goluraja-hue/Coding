50beec6dacb0138da708e38d4c1ce66ce036e117
Goluraja-hue <goluraja7688@gmail.com>
2026-05-03 23:37:49 +0530

    Api axios complete

--- Axios/src/Components/User.jsx
+++ Axios/src/Components/User.jsx
@@ -2,13 +2,10 @@
 const User = (res) => {
 
 
-
-    const clr = Math.floor(Math.random() * 256);
-    const clr1 = Math.floor(Math.random() * 256);
-    const clr2 = Math.floor(Math.random() * 256);
-
-
-  return (
+    const clr = Math.floor(Math.random() * 256)
+    const clr1 = Math.floor(Math.random() * 256)
+    const clr2 = Math.floor(Math.random() * 256)
+    return (
     <div style={{backgroundColor:`rgb(${clr},${clr1},${clr2})`}} className="user-card">
         <img src={res.elem.download_url} alt="" />
         <h3>{res.elem.author}</h3>
